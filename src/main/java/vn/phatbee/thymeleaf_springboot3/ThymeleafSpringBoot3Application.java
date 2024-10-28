df0f575252649b17a31ad4e43027e64ce5fa34ae
PhatBee <vinhphatst1235@gmail.com>
2024-10-28 07:53:03 +0700

    v0, Dependency

---
+++ src/main/java/vn/phatbee/thymeleaf_springboot3/ThymeleafSpringBoot3Application.java
@@ -0,0 +1,13 @@
+package vn.phatbee.thymeleaf_springboot3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafSpringBoot3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafSpringBoot3Application.class, args);
+	}
+
+}
---
+++ src/test/java/vn/phatbee/thymeleaf_springboot3/ThymeleafSpringBoot3ApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.phatbee.thymeleaf_springboot3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafSpringBoot3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
